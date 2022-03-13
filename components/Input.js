e262404409df1551b71692b96bdf4115167d52d2
Madoski254 <gabrielmadolio@gmail.com>
2022-03-13 11:30:26 +0300

    feed changes

--- components/Input.js
+++ components/Input.js
@@ -98,7 +98,8 @@ const Input = () => {
                {selectedFile && (
             <div className="relative">
               <div
-                className="absolute w-8 h-8 bg-[#15181c] hover:bg-[#272c26] bg-opacity-75 rounded-full flex items-center justify-center top-1 left-1 cursor-pointer"
+                className="absolute w-8 h-8 bg-[#15181c] hover:bg-[#272c26] bg-opacity-75 
+                rounded-full flex items-center justify-center top-1 left-1 cursor-pointer"
                 onClick={() => setSelectedFile(null)}
               >
                 <XIcon className="text-white h-5" />
@@ -154,7 +155,9 @@ const Input = () => {
 
               </div>
               <button
-              className="bg-[#1d9bf0] text-white rounded-full px-4 py-1.5 font-bold shadow-md hover:bg-[#1a8cd8] disabled:hover:bg-[#1d9bf0] disabled:opacity-50 disabled:cursor-default"
+              className="bg-[#1d9bf0] text-white rounded-full px-4 py-1.5 font-bold shadow-md 
+              hover:bg-[#1a8cd8] disabled:hover:bg-[#1d9bf0] disabled:opacity-50 
+              disabled:cursor-default"
               disabled={!input && !selectedFile}
               onClick={sendPost}
             >
